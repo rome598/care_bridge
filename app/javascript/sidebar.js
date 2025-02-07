c17e68a38b302e6d65f3493029e506f4a1738c2d
rome598 <eerogm98@gmail.com>
2025-02-07 09:47:18 -0600

    Sidebar structure.

---
+++ app/javascript/sidebar.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Archivo custom.js cargado correctamente.");
+  
+    const toggleButton = document.getElementById('toggle-btn');
+    const sidebar = document.getElementById('sidebar');
+  
+    function toggleSidebar(){
+      sidebar.classList.toggle('close');
+      toggleButton.classList.toggle('rotate');
+      closeAllSubMenus();
+    }
+  
+    function toggleSubMenu(button) {
+      const subMenu = button.nextElementSibling;
+      if (!subMenu.classList.contains('show')) {
+        closeAllSubMenus();
+      }
+  
+      subMenu.classList.toggle('show');
+      button.classList.toggle('rotate');
+  
+      if (sidebar.classList.contains('close')) {
+        sidebar.classList.toggle('close');
+        toggleButton.classList.toggle('rotate');
+      }
+    }
+  
+    function closeAllSubMenus() {
+      Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show');
+        ul.previousElementSibling.classList.remove('rotate');
+      });
+    }
+  
+    toggleButton.addEventListener("click", toggleSidebar);
+  
+    const dropdownButtons = document.querySelectorAll('.dropdown-btn');
+    dropdownButtons.forEach(button => {
+      button.addEventListener('click', function() {
+        toggleSubMenu(button);
+      });
+    });
+});
